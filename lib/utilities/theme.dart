7fec8af576f0c2a5c91c65ceb37ef6956a3db66e
mostafa <mostafaahmederaqy@gmail.com>
2023-06-18 17:05:53 +0400

    My First Commit

---
+++ lib/utilities/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData buildTheme(brightness) {
+  var baseTheme = ThemeData(brightness: brightness);
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.openSansTextTheme(baseTheme.textTheme),
+  );
+}
